6dd6ef3ac62aa79cb21eabadf1c28b6940fc3cb1
Dhrubesh <dhrubesh@trycinema.com>
2017-04-21 23:39:20 +0530

    *now*

--- logic.js
+++ logic.js
@@ -1,6 +1,7 @@
 var graduationDate = new Date("Jan 5, 2018 15:37:25").getTime();
 	var x = setInterval(function() {
 		var now = new Date().getTime();
+		console.log(now)
 		var timeLeft = graduationDate - now;
 
 		var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
