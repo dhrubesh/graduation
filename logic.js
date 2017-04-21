447e78b68dfe1640c95fd40520c89974bced3feb
Dhrubesh <dhrubesh@trycinema.com>
2017-04-21 23:21:51 +0530

    initial set-up  to check up.

---
+++ logic.js
@@ -0,0 +1,17 @@
+var graduationDate = new Date("Jan 5, 2018 15:37:25").getTime();
+	var x = setInterval(function() {
+		var timeLeft = graduationDate - now;
+
+		var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+		document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+		+ minutes + "m " + seconds + "s ";
+
+		if (timeLeft < 0) {
+		clearInterval(x);
+		document.getElementById("countDown").innerHTML = "Graduated! Hurray.";
+		}
+		}, 1000);
